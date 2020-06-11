d21a5832c3bca733ce857ccdfff97f2dbeffda7f
sujeet kumar <sujeetkumar2010@live.com>
2020-06-11 10:02:59 +0530

    Add project files.

---
+++ gitazuredemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitazuredemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
